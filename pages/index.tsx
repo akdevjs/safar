d2bbff73c6474eef5531baa39383fc44855bc71e
Abdullah Khan <abdullah.khan53145@gmail.com>
2023-04-26 19:33:18 -0700

    deployment attempt #10

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import { NextPage } from "next";
-import Hero from "components/Hero";
-import Header from "components/Header";
+// import Hero from "pages/components/Hero";
+// import Header from "pages/components/Header";
 
 const Home: NextPage = () => {
   return (
     <main className="bg-[#E1E1E1]  h-full">
-      <Header />
-      <Hero />
+      {/* <Header />
+      <Hero /> */}
     </main>
   );
 };
